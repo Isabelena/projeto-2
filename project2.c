14b86363f22b834067acf2f5934327f4c202fb87
Isabelena <vh.ocx@hotmail.com>
2025-03-18 21:46:04 -0300

    Update project2.c

--- project2.c
+++ project2.c
@@ -1,1 +1,23 @@
-#inc
+#include <stdio.h>
+#include <stdlib.h>
+
+//projeto 2
+
+int main
+
+{
+  int numero //isto é uma variável
+
+printf("Digite um número" \n\n);
+
+  scanf( %d, &numero );
+  getchar();
+
+printf("O número é igual a: %d, numero" \n\n);
+
+  system("PAUSE");
+
+return 0;
+
+
+  
